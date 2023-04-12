0f99ec1a9e40b46ee09882643fa5494ec774820f
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-11 22:26:11 -0300

    adding files

---
+++ src/componentes/Testimonio.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
+
+function Testimonio(props) {
+  return(
+    <div className="contenedor-testimonio">
+      <img className='imagen-testimonio'
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+      alt={`foto de ${props.firstName}`}/>
+ 
+      <div className="contenedor-text-testimonio">
+        <p className='nombre-testimonio'><strong>{props.nombre} </strong>en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className='text-testimonio'>"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
